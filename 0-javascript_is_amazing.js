cec943aa702e89fff17c3068ae85743c16750133
joseph segun <josephsegzy@gmail.com>
2025-09-13 19:17:32 +0100

    Final fix for task 0

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,8 +1,6 @@
 
 #!/usr/bin/node
 
-// Create a constant variable with the specified value
 const myVar = "JavaScript is amazing";
 
-// Print the value of the variable to the console
 console.log(myVar);
